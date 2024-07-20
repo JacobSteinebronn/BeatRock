d2fe9f27788cee96a4d412ddf634ae26c67e97b4
Jacob Steinebronn <jakesteinebronn@gmail.com>
2024-07-19 20:11:31 -0700

    install flask in script

--- start_fproxy.sh
+++ start_fproxy.sh
@@ -1,3 +1,4 @@
 curl -o /home/jakesteinebronn/fproxy.py https://raw.githubusercontent.com/JacobSteinebronn/BeatRock/main/fproxy.py
 cd /home/jakesteinebronn
+sudo apt install python3-flask
 python3 ./fproxy.py
