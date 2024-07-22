e83c833e89d0303bdef3bf3656e84eefa3a60eb6
Jacob Steinebronn <jakesteinebronn@gmail.com>
2024-07-21 18:03:07 -0700

    update start fproxy script

--- start_fproxy.sh
+++ start_fproxy.sh
@@ -1,6 +1,6 @@
 curl -o /home/jakesteinebronn/fproxy.py https://raw.githubusercontent.com/JacobSteinebronn/BeatRock/main/fproxy.py
 cd /home/jakesteinebronn
 sudo apt install python3-flask
-sudo apt install python3-httpx
-sudo apt install python3-aiohttp
+sudo apt install python3-httpx -y
+sudo apt install python3-aiohttp -y
 python3 ./fproxy.py
